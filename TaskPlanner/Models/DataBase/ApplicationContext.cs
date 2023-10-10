b9b043f38b245098d39ffd98622492040aa9fa28
alexandrsotnickov <alexandr.sotnickov@gmail.com>
2023-10-10 21:37:01 +0700

    got everything ready for the database migration

---
+++ TaskPlanner/Models/DataBase/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskPlanner.Models.Entities;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Calendar> Calendars => Set<Calendar>();
+    public DbSet<CalendarEntry> calendarEntries => Set<CalendarEntry>();
+    public DbSet<Challenge> challenges => Set<Challenge>();
+    public DbSet<Event> events => Set<Event>();
+    public DbSet<Note> notes => Set<Note>();
+  
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS);Database=helloappdb;Trusted_Connection=True;");
+    }
+}
